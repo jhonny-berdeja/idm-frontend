27b42c45698104babc1af19564e35a91ebf90da0
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-12-01 14:40:35 -0300

    implemented routers basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Login from './pages-view/Login';
+import { Navigate, Route, Router, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Navigate to="/login" />} />
+    </Routes>
   );
 };
 
---
+++ src/pages-view/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+  return <h1>¡Hola, esta es la Hola Page!</h1>;
+};
+
+export default Login;
