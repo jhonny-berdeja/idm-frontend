da203c4973d85750b59666c591b7852f7d6c1ac7
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-12-02 14:15:47 -0300

    Home.jsx | implemented view

--- src/pages-view/Home.jsx
+++ src/pages-view/Home.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import '../styles/Login.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>Only users with the "COMMON" role can see this page.</p>
+    <div className="login-container" >
+        <Container>
+          <Row>
+            <Col sm>
+              <div className="center-horizontal form-bordered">
+                <Button variant="dark">
+                  <h1>
+                    Access Management
+                  </h1>
+                </Button>
+              </div>
+            </Col>
+            <Col sm>
+              <div className="center-horizontal form-bordered">
+                <Button variant="dark">
+                  <h1>
+                  Human Resources
+                  </h1>
+                </Button>                
+              </div>
+            </Col>
+            <Col sm>
+              <div className="center-horizontal form-bordered">
+                <Button variant="dark">
+                  <h1>
+                    Access Audit
+                  </h1>
+                </Button>                   
+              </div>
+            </Col>
+          </Row>
+        </Container>
     </div>
   );
 };
