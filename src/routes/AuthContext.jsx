f1345b097f02ff0a64553ff3c6c6d0bd4b0c3363
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-12-02 16:55:49 -0300

    AuthContext.jsx | implemented persistence of authentication status in localStore

--- src/routes/AuthContext.jsx
+++ src/routes/AuthContext.jsx
@@ -1,26 +1,38 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    isAuthenticated: false,
-    role: null,
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem('user');
+    return storedUser? JSON.parse(storedUser) : { isAuthenticated: false, role: null };
   });
 
   const login = (role) => {
-    setUser({
+    const newUser = {
       isAuthenticated: true,
       role,
-    });
+    };
+    setUser(newUser);
+    localStorage.setItem('user', JSON.stringify(newUser));
   };
 
   const logout = () => {
-    setUser({
+    const newUser = {
       isAuthenticated: false,
       role: null,
-    });
+    };
+    setUser(newUser);
+    localStorage.removeItem('user');
   };
+
+  useEffect(() => {
+    if (user.isAuthenticated) {
+      localStorage.setItem('user', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('user');
+    }
+  }, [user]);
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
